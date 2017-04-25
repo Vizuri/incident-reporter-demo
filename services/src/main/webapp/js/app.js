2505da74c00cb2554db0fac7e87f95f713dd1bcb
Ken Spokas <kspokas@vizuri.com>
2017-04-25 03:25:04 -0400

    Updated settings for demo app

--- services/src/main/webapp/js/app.js
+++ services/src/main/webapp/js/app.js
@@ -4,7 +4,7 @@
 	angular.module('bpmsFlowApp', [ 'bpmsFlowApp.controllers' ]);
 
 	angular.module('bpmsFlowApp').config([ '$httpProvider', function($httpProvider) {
-		$httpProvider.defaults.headers.common['Authorization'] = 'Basic ZGVjaWRlcjpkZWNpZGVyIzk5';
+		$httpProvider.defaults.headers.common['Authorization'] = 'Basic cHJvY2Vzc29yOnByb2Nlc3NvciM5OQ==';
 	} ]);
 
 })();
